5201a5a51f2f90d660e13146d5e31957acf411db
Khaled <khaledsahli36@gmail.com>
2023-06-02 18:24:47 +0100

    Not required

--- users-service/Models/User.js
+++ users-service/Models/User.js
@@ -1,20 +1,20 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-  uid: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  tel: { type: String, required: true },
-  adresse: { type: String, required: true },
+  uid: { type: String, unique: true, default: null },
+  email: { type: String, unique: true, default: null },
+  password: { type: String, default: null },
+  tel: { type: String, default: null },
+  adresse: { type: String, default: null },
   role: { type: String, enum: ['Gerant', 'Comptable', 'Admin', 'Super_Admin'] },
-  photo: { type: String },
+  photo: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
   deletedAt: { type: Date, default: null },
   blockedAt: { type: Date, default: null },
-  dateNaissance: { type: Date, required: true },
-  cp: { type: String, required: true },
-  ville: { type: String, required: true },
-  civilité: { type: String, required: true },
+  dateNaissance: { type: Date, default: null },
+  cp: { type: String, default: null },
+  ville: { type: String, default: null },
+  civilité: { type: String, default: null },
 })
 
 const User = model('user', userSchema)
