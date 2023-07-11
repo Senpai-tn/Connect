a184cd049648ea45232e3557a3a89e80bbcb7a9f
Khaled <khaledsahli36@gmail.com>
2023-07-11 15:18:45 +0100

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 app.use(express.json())
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://app.ccconnect.fr') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
