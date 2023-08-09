45cbd939a821d549d08d08596660df885b432fa6
Khaled <khaledsahli36@gmail.com>
2023-08-09 19:48:18 +0100

    axios

--- users-service/routes/auth.js
+++ users-service/routes/auth.js
@@ -5,17 +5,7 @@ const bcrypt = require('bcryptjs')
 var jwt = require('jsonwebtoken')
 const node_xlsx = require('node-xlsx')
 const saltRounds = 10
-const nodemailer = require('nodemailer')
-const transporter = nodemailer.createTransport({
-  host: 'smtp.forwardemail.net',
-  port: 465,
-  secure: true,
-  auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: 'REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM',
-    pass: 'REPLACE-WITH-YOUR-GENERATED-PASSWORD',
-  },
-})
+
 const User = require('../models/user')
 
 const router = express.Router()
