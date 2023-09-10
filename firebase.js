ad153f9e0f91797033d0fb430d0eb4bfce853ddb
Khaled Sahli <60314408+Senpai-tn@users.noreply.github.com>
2023-09-10 16:43:35 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ const { getFirestore } = require('firebase/firestore')
 const { getStorage } = require('firebase/storage')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCNPA21TL-EUhkbRM3UXHg7izXRpp46IlM',
-  authDomain: 'connect-ef1c5.firebaseapp.com',
-  projectId: 'connect-ef1c5',
-  storageBucket: 'connect-ef1c5.appspot.com',
-  messagingSenderId: '564194901088',
-  appId: '1:564194901088:web:8e0df71baa23f0f4769705',
+  
 }
 
 const app = initializeApp(firebaseConfig)
